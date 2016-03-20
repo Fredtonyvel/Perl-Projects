e5d07bf27bf04e65c52463cd5a88fc6bf1274a01
Gary Tsai <garytsai@Garys-MacBook-Pro.local>
2016-03-20 19:37:50 -0400

    finished Part2 #3. Please look for comment.

--- p2.pl
+++ p2.pl
@@ -1,34 +1,46 @@
 #/usr/local/bin/perl
+use strict;
+use warnings;
+
 
 open(INPUT, "<electricity.txt");
-$file = 1;
-# question 1
+
+my %count;# i had this line of code under while loop. Hence, it keeps give me trouble
 
 
-@d=qw(you are awesome);
+while (my $sentence=<INPUT>) {
 
-print (sort @d\n);
-
-while (<INPUT>) {
-
-	$sentence= $_ ;
-	@word=split(/ /, $sentence);
+	process(\$sentence);
+	$sentence =~s/\W/ /gi; # this line is to remve all non alphanumeric
 	
+	my @sentence = split /\s+/,$sentence;
 
-	$sentence =~ s/\W/ /gi;
-	$sentence=~ s/\s+/ /g;
-	
-	@sentence = split(//,$sentence);
-
-
-	
-	#print $sentence;
-	
-	#print sort{ $a cmp $b } @sentence;
-
-	
-
-
+	foreach my $str (@sentence){
+		$count{$str}++;
 	}
+	#$sentence =~ s/\W/ /gi;
+	#$sentence=~ s/\s+/ /g;
+	#process(\$sentence);
+	
+
+
+	foreach my $str (sort keys %count) {
+   printf "%-31s %s\n", $str, $count{$str};
+}
+
+	#print "@sentence\n";
+	
+	#print (sort @sentence);
+
+	
+
+}
+
+sub process {# this function is to remove all "\n" (new line) character and long space
+    my $sentence = shift;
+    $sentence=~s/\n//g;
+    $sentence=~s/\r//g;
+    return $sentence;
+}
 
 close (INPUT);
