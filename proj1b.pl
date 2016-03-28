a8ce85079d16e31e26bcb2a630458b94c1b18d85
fredtonyvel <vfredanthony@yahoo.com>
2016-03-27 20:45:58 -0400

    Finished part 2!

--- proj1b.pl
+++ proj1b.pl
@@ -26,4 +26,76 @@ if ($command == 1)
 	}
 }
 
+if ($command == 2)
+{
+	print "Part2: Instruction 2: \n\n";
+	print "File sorted alphabetically with upper case words in front of lower case words:\n";
+
+	my %hash;
+
+	while ($word = <FH>)
+	{
+		$word =~ s/[^a-z]/ /gi;		#removes all non-alphanumeric characters
+		
+		foreach $line (split /\s+/, $word)		#splits by removing all spaces between words
+		{
+			$hash{$line}++;				#increment hash count
+			#print "$line\n";
+		}
+
+		foreach $line (sort keys %hash)		#sorts alphabetically with uc infront of lc
+		{
+			print "$line\n";
+		}
+	}
+}
+
+if ($command == 3)
+{
+	print "Part2: Instruction 3: \n\n";
+	print "Frequency from high to low:\n";
+
+	my %hash;
+
+	while ($word = <FH>)
+	{
+		$word =~ s/[^a-z]/ /gi;		#removes all non-alphanumeric characters
+		
+		foreach $line (split /\s+/, $word)		#splits by removing all spaces between words
+		{
+			$hash{$line}++;				#increment hash count
+			#print "$line\n";
+		}
+
+		foreach $line (sort {$hash{$b} <=> $hash{$a}} keys %hash)		#sorts words by frequency from high to low
+		{
+			printf "$line ==> $hash{$line}\n";
+		}
+	}
+}
+
+if ($command == 4)
+{
+	print "Part2: Instruction 4: \n\n";
+	print "Frequency by alphabetical order:\n";
+
+	my %hash;
+
+	while ($word = <FH>)
+	{
+		$word =~ s/[^a-z]/ /gi;		#removes all non-alphanumeric characters
+		
+		foreach $line (split /\s+/, $word)		#splits by removing all spaces between words
+		{
+			$hash{$line}++;				#increment hash count
+			#print "$line\n";
+		}
+
+		foreach $line (sort keys %hash)		#sorts alphabetically by frequency
+		{
+			printf "$line ==> $hash{$line}\n";	#prints sorted words and frequency by alphabetical order
+		}
+	}
+}
+
 close (FH);
