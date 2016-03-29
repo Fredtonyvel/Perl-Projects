4cf4e25a2758d5db7d22324b574beddb107f9dce
garymalaysia <gtsai70@gmail.com>
2016-03-29 13:48:11 -0400

    checj for study.cpp.

---
+++ study.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+int pow (int,int);
+
+int main(int argc, char *argv[]) {
+
+	int k, base, ex, num;
+	cout<< "enter a number" << endl; 
+	cin >> base;
+	cout<< " enter power "<< endl;
+	cin >> ex;
+	num =1;
+	int ans=0;
+	
+
+		if (ex == 0)
+			cout << "Answer is "<< num<<endl;
+		else
+			base *= base;
+	}
+	cout<< base;
+
+}
+
+int pow (int x, )
+
